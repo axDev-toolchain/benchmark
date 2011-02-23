8c231164b9a0a1b1b0784fe6d376ef201dce433d
Jing Yu <jingyu@google.com>
2011-02-22 19:42:37 -0800

    Update eclair header and lib map to support python benchmark.
    Change-Id: I0d8e76732c38365bc411f29110a53ba6425a54d0

--- android_build/eclair/bionic/libc/include/locale.h
+++ android_build/eclair/bionic/libc/include/locale.h
@@ -52,7 +52,26 @@ enum {
 extern char *setlocale(int category, const char *locale);
 
 /* Make libstdc++-v3 happy.  */
-struct lconv { };
+struct lconv {
+  char *decimal_point;
+  char *thousands_sep;
+  char *grouping;
+  char *int_curr_symbol;
+  char *currency_symbol;
+  char *mon_decimal_point;
+  char *mon_thousands_sep;
+  char *mon_grouping;
+  char *positive_sign;
+  char *negative_sign;
+  char  int_frac_digits;
+  char  frac_digits;
+  char  p_cs_precedes;
+  char  p_sep_by_space;
+  char  n_cs_precedes;
+  char  n_sep_by_space;
+  char  p_sign_posn;
+  char  n_sign_posn;
+};
 struct lconv *localeconv(void);
 
 __END_DECLS
